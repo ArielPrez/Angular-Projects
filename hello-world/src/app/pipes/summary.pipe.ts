0e06803f45f623b9189c0d64ec896dc4813864fe
ArielPrez <arielpgmm@gmail.com>
2018-09-27 00:00:15 -0400

    pipes

--- hello-world/src/app/pipes/summary.pipe.ts
+++ hello-world/src/app/pipes/summary.pipe.ts
@@ -8,7 +8,7 @@ export class SummaryPipe implements PipeTransform {
   transform(value: string, limit?: number) {
     if(!value)
       return null;
-    let actualLimit = (limit) ? limit : 50;
+    let actualLimit = (limit) ? limit : 10;
     return value.substr(0, actualLimit)+'...';
   }
 
