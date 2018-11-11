9ecca7c1b392b206a644d0d388b5b37509f63bc6
ArielPrez <arielpgmm@gmail.com>
2018-11-10 23:25:01 -0500

    Reactive Form => signup-Form with Asynchronous validation errors

--- hello-world/src/app/common/validators/username.validators.ts
+++ hello-world/src/app/common/validators/username.validators.ts
@@ -2,8 +2,24 @@ import { AbstractControl, ValidationErrors } from "@angular/forms";
 
 export class UsernameValidators{
   static cannotContainSpace(control: AbstractControl) : ValidationErrors | null {
-    if((control.value as string).indexOf(' ') >= 0)
-      return { cannotContainSpace: true };
+    if((control.value as string).indexOf(' ') >= 0){
+      return { cannotContainSpace: true, minlength:{requiredLength: 3, actualLength: control.value.length} };
+    }
     return null;
+  }
+
+// ASYNCHRONOUS OPERATION TO SIMULATE A CALL TO THE SERVER TO CHECK IF THE USER IS TAKEN
+  static shouldBeUnique(control: AbstractControl) : Promise<ValidationErrors | null> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if(control.value === 'Ariel'){
+          resolve({ shouldBeUnique: true });
+        }else{
+          resolve (null);
+        }
+      }, 2000);
+ 
+    });
+     
   }
 }
--- hello-world/src/app/signup-form/signup-form.component.ts
+++ hello-world/src/app/signup-form/signup-form.component.ts
@@ -11,8 +11,9 @@ export class SignupFormComponent {
   form = new FormGroup({
     username: new FormControl('', [
       Validators.required, 
-      Validators.minLength(3),
-      UsernameValidators.cannotContainSpace
+      Validators.minLength(5),
+      UsernameValidators.cannotContainSpace,
+      UsernameValidators.shouldBeUnique
     ]),
     password: new FormControl('', Validators.required)
   });
