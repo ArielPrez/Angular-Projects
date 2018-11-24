afdf359c475829fe8f39b72019f623a6b8f56635
ArielPrez <arielpgmm@gmail.com>
2018-11-23 23:31:11 -0500

    posts Component - Updating Data from a JSONPlaceHolder Example

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-posts',
@@ -30,8 +31,15 @@ export class PostsComponent implements OnInit {
        text['id'] = response.json().id;
         // SPLICE - TO ADD IT AT THE BEGINNING OF THE LIST, INSTEAD USING THE PUSH TO ADD IT TO THE END
         this.posts.splice(0, 0, text);
-        
       });
+  }
+  updatePost(post){
+    //USING THE PATCH METHOD TO UPDATE ONLY A FEW PROPERTIES IN AN OBJECT INSTEAD OF SENDING THE COMPLETE OBJECT TO THE SERVER WITH PUT
+    // this.http.put(this.url, JSON.stringify(post))
+    this.http.patch(this.url + '/' + post.id, JSON.stringify({isRead: true}))
+      .subscribe(response => {
+        console.log(response.json());
+      })
   }
 
 }
