ed838ad3f99044625b8616b93fc69499f2719e03
ArielPrez <arielpgmm@gmail.com>
2018-11-24 19:37:11 -0500

    posts Component - Deleting Data from a JSONPlaceHolder Example

--- hello-world/src/app/posts/posts.component.ts
+++ hello-world/src/app/posts/posts.component.ts
@@ -11,17 +11,19 @@ export class PostsComponent implements OnInit {
 
   posts: any[];
   private url = 'https://jsonplaceholder.typicode.com/posts';
-  constructor(private http: Http) { 
-    http.get(this.url)
+  
+  constructor(private http: Http) {
+  }
+  
+  // READ
+  ngOnInit() {
+    // DON'T CALL HTTP SERVICES ON THE CONSTRUCTOR OF THE COMPONENT FOR INITIALIZATION
+    this.http.get(this.url)
     .subscribe( response => {
       this.posts = response.json();
     });
-
   }
-
-  ngOnInit() {
-  }
-
+  // CREATE
   createPost(input: HTMLInputElement){
     let text = {title: input.value};
     input.value = '';
@@ -33,6 +35,7 @@ export class PostsComponent implements OnInit {
         this.posts.splice(0, 0, text);
       });
   }
+  // UPDATE
   updatePost(post){
     //USING THE PATCH METHOD TO UPDATE ONLY A FEW PROPERTIES IN AN OBJECT INSTEAD OF SENDING THE COMPLETE OBJECT TO THE SERVER WITH PUT
     // this.http.put(this.url, JSON.stringify(post))
@@ -41,5 +44,14 @@ export class PostsComponent implements OnInit {
         console.log(response.json());
       })
   }
-
+  // DELETE
+  deletePost(post){
+    //BY CONVENTION THE HTTP.DELETE REQUEST DON'T HAVE A BODY
+    this.http.delete(this.url + '/' + post.id)
+      .subscribe(response => {
+        let index = this.posts.indexOf(post);
+        // let index = response.json().id;
+        this.posts.splice(index, 1);
+      });
+  }
 }
