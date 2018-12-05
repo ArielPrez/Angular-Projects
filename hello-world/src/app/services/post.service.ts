bed69933e72854eccb5877821b3d9da549789023
ArielPrez <arielpgmm@gmail.com>
2018-12-04 23:15:21 -0500

    new Data Service - Testing Throw Errors - now is working

--- hello-world/src/app/services/post.service.ts
+++ hello-world/src/app/services/post.service.ts
@@ -8,7 +8,7 @@ import { DataService } from './data.service';
 })
 export class PostService extends DataService {
 
-  private url = 'https://jsonplaceholder.typicode.com/posts';
+  // private url = 'https://jsonplaceholder.typicode.com/posts';
 
   constructor(http: Http) { 
     super('https://jsonplaceholder.typicode.com/posts',http);
