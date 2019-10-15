1b7a6320cbfeb1f7951ef20fe0c341e84aaa544e
ArielPrez <arielpgmm@gmail.com>
2019-10-14 22:13:08 -0400

    followers.service.ts - error solved

--- hello-world/src/app/services/followers.service.ts
+++ hello-world/src/app/services/followers.service.ts
@@ -3,10 +3,12 @@ import { DataService } from './data.service';
 import { Http } from '@angular/http';
 
 @Injectable()
-export class FollowersService extends DataService{
+export class FollowersService extends DataService {
 
-  constructor(http: Http) { 
+  constructor(http: Http) {
     super('https://tinyfac.es/api/users', http);
-}
+  }
 
 // https://uifaces.co/api?limit=10&emotion[]=happiness
+}
+
